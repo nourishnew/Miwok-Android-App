f85672f14a5f50312b340a1a96cf3b820392dbe1
NOURISH CHERISH <nourishnew@gmail.com>
2018-11-22 15:03:02 -0500

    Update ColorsActivity.java

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -14,15 +14,16 @@ public class ColorsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_colors);
 
 
-        ArrayList<Word> colors=new ArrayList<Word>();
-        colors.add(new Word("red" ,"weṭeṭṭi"));
-        colors.add(new Word("green", "chokokki"));
-        colors.add(new Word("brown", "ṭakaakki"));
-        colors.add(new Word("gray", "ṭopoppi"));
-        colors.add(new Word("black", "kululli"));
-        colors.add(new Word("white", "kelelli"));
-        colors.add(new Word("dusty yellow", "ṭopiisә"));
-        colors.add(new Word("mustard yellow", "chiwiiṭә"));
+      ArrayList<Word> colors=new ArrayList<Word>();
+        colors.add(new Word("red", "weṭeṭṭi", R.drawable.color_red));
+        colors.add(new Word("mustard yellow", "chiwiiṭә", R.drawable.color_mustard_yellow));
+        colors.add(new Word("dusty yellow", "ṭopiisә", R.drawable.color_dusty_yellow));
+        colors.add(new Word("green", "chokokki", R.drawable.color_green));
+        colors.add(new Word("brown", "ṭakaakki", R.drawable.color_brown));
+        colors.add(new Word("gray", "ṭopoppi", R.drawable.color_gray));
+        colors.add(new Word("black", "kululli", R.drawable.color_black));
+        colors.add(new Word("white", "kelelli", R.drawable.color_white));
+
 
 
         WordAdapter colorAdapter=new WordAdapter(this,colors);
