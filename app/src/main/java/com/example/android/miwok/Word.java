70962fe25f01b08d3831d0fc3eb85093094c057b
NOURISH CHERISH <nourishnew@gmail.com>
2018-12-01 00:37:46 -0500

    adding audioresourcei id to word object

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -4,22 +4,33 @@ import android.content.Context;
 
 public class Word {
 
-   private String mDefaultTranslation;
+    private String mDefaultTranslation;
     private String mMiwokTranslation;
-    private int mImageResourceId;
+    private int mImageResourceId=NO_IMAGE_PROVIDED;
+    private static final int NO_IMAGE_PROVIDED=-1;
+    private int mAudioResourceId;
 
+    @Override
+    public String toString() {
+        return "Word{" +
+                "mDefaultTranslation='" + mDefaultTranslation + '\'' +
+                ", mMiwokTranslation='" + mMiwokTranslation + '\'' +
+                ", mImageResourceId=" + mImageResourceId +
+                ", mAudioResourceId=" + mAudioResourceId +
+                '}';
+    }
 
-
-
-    public Word(String defaultTransition, String miwokTransition, int imageResourceId){
+    public Word(String defaultTransition, String miwokTransition, int imageResourceId, int audioResourceId){
         mDefaultTranslation=defaultTransition;
         mMiwokTranslation=miwokTransition;
         mImageResourceId=imageResourceId;
+        mAudioResourceId=audioResourceId;
 
     }
-    public Word(String defaultTransition, String miwokTransition){
+    public Word(String defaultTransition, String miwokTransition,int audioResourceId){
         mDefaultTranslation=defaultTransition;
         mMiwokTranslation=miwokTransition;
+        mAudioResourceId=audioResourceId;
 
 
     }
@@ -32,9 +43,17 @@ public class Word {
     public String getMiwokTranslation() {
         return mMiwokTranslation;
     }
+    public int getAudioResourceId(){
+        return mAudioResourceId;
+    }
 
     public int getimageResourceId() {
         return mImageResourceId;
     }
+    public boolean hasImage() {
+        return mImageResourceId != NO_IMAGE_PROVIDED;
+    }
+
+
 }
 
